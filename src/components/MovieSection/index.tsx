036b7b0282d8cf5a96adac65126511aa7881fb8f
Diogo CS Berner <diogoberner@gmail.com>
2025-05-04 19:24:52 -0300

    feat: implement custom hooks for movie fetching and filtering in MovieSection component

--- src/components/MovieSection/index.tsx
+++ src/components/MovieSection/index.tsx
@@ -4,37 +4,33 @@ import Fieldset from "../Fieldset";
 import InputText from "../InputText";
 import styles from "./MovieSection.module.css";
 import MoviesList from "../MoviesList";
-import { Movie } from "../../types";
-import { useEffect, useState } from "react";
-import { getMovies } from "../../api";
+import useFetchMovies from "../../hooks/useFetchMovies";
+import useFilterMovies from "../../hooks/useFilterMovies";
 
 const MovieSection = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-
-  const fetchMovies = async () => {
-    try {
-      const movies = await getMovies();
-      setMovies(movies);
-    } catch (err) {
-      console.error("Erro ao buscar filmes..." + err);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovies();
-  }, []);
+  const { movies, isLoading, error } = useFetchMovies();
+  const { searchTerm, setSearchTerm, filteredMovies } = useFilterMovies(
+    movies,
+    (movie) => movie.title
+  );
 
   return (
     <main>
       <section className={styles.container}>
         <Fieldset variant="secondary">
-          <InputText placeholder="Buscar filmes..." />
+          <InputText
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Buscar filmes..."
+          />
           <Button variant="icon">
             <FaSearch />
           </Button>
         </Fieldset>
         <h1 className={styles.title}>Em cartaz</h1>
-        <MoviesList movies={movies} />
+        {isLoading && <p>Carregando...</p>}
+        {error && <p className={styles.error}>{error}</p>}
+        <MoviesList movies={filteredMovies} />
       </section>
     </main>
   );
---
+++ src/hooks/useFilterMovies.ts
@@ -0,0 +1,19 @@
+import { useMemo, useState } from "react";
+import { Movie } from "../types";
+
+const useFilterMovies = (
+  movies: Movie[],
+  getValue: (movie: Movie) => string
+) => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const filteredMovies: Movie[] = useMemo(() => {
+    return movies.filter((movie) =>
+      getValue(movie).toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [movies, searchTerm, getValue]);
+
+  return { searchTerm, setSearchTerm, filteredMovies };
+};
+
+export default useFilterMovies;
